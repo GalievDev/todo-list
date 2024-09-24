89bdb90596ab69785f39b046c3c3aa775836b69f
Vazir Vadirov <ggvalion@gmail.com>
2024-09-24 17:44:57 +0300

    Task model and repository

---
+++ src/main/java/dev/galiev/todolist/model/Task.java
@@ -0,0 +1,20 @@
+package dev.galiev.todolist.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
---
+++ src/main/java/dev/galiev/todolist/repository/TasksRepository.java
@@ -0,0 +1,9 @@
+package dev.galiev.todolist.repository;
+
+import dev.galiev.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Task, Long> {
+}
