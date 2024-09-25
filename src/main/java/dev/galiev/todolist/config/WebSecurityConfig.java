2ce47b3a7aa980830e7027b46bafb5a8c3eb83c2
Vazir Vadirov <ggvalion@gmail.com>
2024-09-25 13:57:21 +0300

    Permissions changed

--- src/main/java/dev/galiev/todolist/config/WebSecurityConfig.java
+++ src/main/java/dev/galiev/todolist/config/WebSecurityConfig.java
@@ -22,13 +22,25 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class WebSecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/api/v1/tasks/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers("/api/v1/auth/**")
+                    req.requestMatchers(WHITE_LIST_URL)
                             .permitAll()
                             .requestMatchers("/api/v1/users/**").hasAnyRole(Role.USER.name())
                             .anyRequest()
